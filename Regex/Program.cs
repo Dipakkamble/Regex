4bc488eda086711939ffbdb0316568a4fc5795e6
Dipakkamble <kambledipak700@gmail.com>
2023-02-18 22:17:56 +0530

    [Dipak] Modified validate first name

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using Regexproblem;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Regex
+namespace RegexProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to regex");
+            UserRegistration userRegistration=new UserRegistration();
+            Console.Write("Enter the first name :");
+            string firstname=Console.ReadLine();
+            userRegistration.ValidateFirstName(firstname);
             Console.ReadLine();
+           
         }
     }
 }
---
+++ Regex/UserRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Regexproblem
+{
+    internal class UserRegistration
+    {
+        public string FirstName = ("^[A-Z]{1}[a-z]{2,}$");
+        public void ValidateFirstName(string firstName)
+        {
+            if (Regex.IsMatch(firstName, FirstName))
+            {
+                Console.WriteLine("{0} is Valid " ,firstName);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Invalid " ,firstName);
+            }
+        }
+    }
+}
